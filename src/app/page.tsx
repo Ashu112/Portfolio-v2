7ea7e4a14cc57b4b0b5d8a4fc0a42217fb38ed4e
Ashu112 <kashu2611@gmail.com>
2026-01-16 03:35:52 +0530

    tailwind css added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <div>
       <main>
-       <h1>Portfolio v2</h1>
+       <h1 className="text-3xl text-yellow-300">Portfolio v2</h1>
       </main>
     </div>
   );
