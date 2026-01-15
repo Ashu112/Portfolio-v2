a655cd8f703fd6230fed58742fee1024f57832a3
Ashu112 <kashu2611@gmail.com>
2026-01-16 03:44:36 +0530

    tailwind test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
     <div>
       <main>
        <h1 className="text-3xl text-yellow-300">Portfolio v2</h1>
+       <h2 className="text-[100px] text-green-400">Tailwind Css</h2>
       </main>
     </div>
   );
