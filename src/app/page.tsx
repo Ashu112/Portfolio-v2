81ac821dada8d52649af6c93d8099575645258fb
Ashu112 <kashu2611@gmail.com>
2026-01-17 04:07:56 +0530

    Fonts and tokens added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,6 @@ export default function Home() {
   return (
     <div>
       <main>
-       <h1 className="text-3xl text-yellow-300">Portfolio v2</h1>
-       <h2 className="text-[100px] text-green-400">Tailwind Css</h2>
       </main>
     </div>
   );
