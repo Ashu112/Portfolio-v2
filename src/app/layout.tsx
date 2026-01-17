8af23bf6f5b82799146d071140f303f1fa0f2e48
Ashu112 <kashu2611@gmail.com>
2026-01-17 09:43:27 +0530

    Header created

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter,Space_Grotesk } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -27,6 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+         <Header/>
         {children}
       </body>
     </html>
