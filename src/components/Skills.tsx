4a60976fa413b2ff24889b14015db6b757d8b3ac
Ashu112 <kashu2611@gmail.com>
2026-01-23 20:19:29 +0530

    skills skeleton

---
+++ src/components/Skills.tsx
@@ -0,0 +1,16 @@
+export default function Skills() {
+  return (
+    <section className="min-h-screen">
+      <div className="section-container py-24">
+        <div className="space-y-6">
+          {/* Skills Heading */}
+          <div className="space-y-4"></div>
+          {/* skills grid*/}
+          <div></div>
+          {/* Proficiency level*/}
+          <div></div>
+        </div>
+      </div>
+    </section>
+  );
+}
