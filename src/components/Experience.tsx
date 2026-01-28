ee7bd013070d40e8b2b0675679834cf4f68378a1
Ashu112 <kashu2611@gmail.com>
2026-01-28 20:11:25 +0530

    experience section

---
+++ src/components/Experience.tsx
@@ -0,0 +1,20 @@
+export default function Expereince() {
+  return (
+    <section id="experience" className="min-h-screen py-20">
+      <div className="section-container">
+        {/* text section */}
+        <div className="max-w-3xl space-y-4">
+          <p className="text-primary font-medium">Experience</p>
+          <h2 className="text-5xl font-bold">Professional Journey</h2>
+          <p className="text-muted-foreground leading-relaxed">
+            From optimizing enterprise dashboards to building real-time systems,
+            each role has shaped my approach to crafting scalable, user-focused
+            solutions.
+          </p>
+        </div>
+        {/* timeline section */}
+        <div></div>
+      </div>
+    </section>
+  );
+}
