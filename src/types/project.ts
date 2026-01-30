090925d50fb5d6b0a4a6b80a8bcdefc05387bf33
Ashu112 <kashu2611@gmail.com>
2026-01-30 22:24:39 +0530

    projects data added

---
+++ src/types/project.ts
@@ -0,0 +1,11 @@
+export type ProjectDataType = {
+  id: number;
+  title: string;
+  description: string;
+  longDescription: string;
+  techStack: string[];
+  image: string;
+  github: string;
+  link: string;
+  color: string;
+};
