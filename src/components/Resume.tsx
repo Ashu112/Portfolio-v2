c670da877d8fbd8c8cb49cd83d98dbcad5762340
Ashu112 <kashu2611@gmail.com>
2026-02-04 23:18:55 +0530

    gradient class added

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -8,7 +8,7 @@ export default function Resume() {
           <div className="flex items-center justify-between">
             {/* Resume info */}
             <div className="flex items-center gap-6">
-              <div className="from-gradient-start via-gradient-mid to-gradient-end flex h-16 w-16 items-center justify-center rounded-2xl bg-linear-to-r">
+              <div className="gradient-bg-strong flex h-16 w-16 items-center justify-center rounded-2xl">
                 <FileText className="text-primary-foreground size-8" />
               </div>
               <div className="space-y-1">
@@ -25,7 +25,7 @@ export default function Resume() {
             <a
               href="/Ashutosh_Kumar_Resume.pdf"
               download
-              className="text-primary-foreground inline-flex items-center gap-3 rounded-xl bg-pink-500 px-8 py-4 font-semibold"
+              className="text-primary-foreground gradient-bg-strong inline-flex items-center gap-3 rounded-xl px-8 py-4 font-semibold"
             >
               <ArrowDown className="size-5" />
               Download Resume
