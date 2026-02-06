a377a90d6ed07da6ddf9e47b975043c4138cefe2
Ashu112 <kashu2611@gmail.com>
2026-02-06 22:54:25 +0530

    resume section responsive design fix

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -7,13 +7,13 @@ export default function Resume() {
       <div className="from-gradient-start/10 via-gradient-mid/10 to-gradient-end/10 absolute inset-0 bg-linear-to-r" />
       <div className="section-container">
         <div className="border-border bg-card rounded-3xl border p-10">
-          <div className="flex items-center justify-between">
+          <div className="flex flex-col items-center justify-between gap-4 md:flex-row">
             {/* Resume info */}
-            <div className="flex items-center gap-6">
+            <div className="flex flex-col items-center gap-6 md:flex-row">
               <div className="gradient-bg-strong flex h-16 w-16 items-center justify-center rounded-2xl">
                 <FileText className="text-primary-foreground size-8" />
               </div>
-              <div className="space-y-1">
+              <div className="w-full space-y-1 text-center md:text-start">
                 <h3 className="text-foreground text-2xl font-bold">
                   Want my full resume?
                 </h3>
